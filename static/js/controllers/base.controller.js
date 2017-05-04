6ea8903ee2c1b682caadcd4c1bf940a1711f47ef
santoli-massimo <santoli.massimo@gmail.com>
2017-05-04 15:57:00 +0200

    Added: request membership modal

--- static/js/controllers/base.controller.js
+++ static/js/controllers/base.controller.js
@@ -1,41 +1,37 @@
-export default [ '$scope','$uibModal','$http', function ($scope,$uibModal,$http) {
+export default [ '$scope','$uibModal','$http','$rootScope', function ($scope,$uibModal,$http,$rootScope) {
+    
+    $scope.rootScope = $rootScope;
     
     $scope.openModal = (m) => {
         let modal = $(m);
         if( modal.length>0 ){
             $scope.modalInstance = $uibModal.open({
                 template: modal.html(),
-                controller: 'baseController',
+                controller: 'requestMembershipController',
                 backdrop: 'static',
                 scope: $scope
             });
         }
-
     }
     
-    $scope.closeModal = () => { $scope.modalInstance.close(); $scope.modal_response = 'ciao'; }
+    $scope.closeModal = () => { $scope.modalInstance.close();  $scope.modal_message = null;}
     
-    $scope.modal_response = {};
+    $scope.modal_message = null
     $scope.request_membership = (email) => {
-        
-        console.log('oki');
         
         return $http({
             'method':'GET',
             'url' : '/api/v1.0/request_membership/'+email
         }).then(
-            r => {
-                $scope.modal_response = { message: r.data.message , status : 'success' }
-                console.log('modal_response', $scope.modal_response);
-            }
+            r => {$scope.modal_message = { message: r.data.message , status : 'success' }}
             ,
             r => {
-                if( r.data.hasOwnProperty('message') ) $scope.modal_response = r.data;
-                else $scope.modal_response = { message: r.statusText , status : 'error' }
+                if( r.data.hasOwnProperty('message') ) $scope.modal_message = r.data;
+                else $scope.modal_message = { message: r.statusText , status : 'error' }
             }
         )
+
     }
-    
-    $scope.test = e => console.log($scope.modal_response);
+
     
 }]
