e9a0531fbec0954d1a9b5cc7bd55314544422998
alexcomu <alex.comunian@gmail.com>
2017-05-08 18:12:44 +0200

    profile class on admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'picture_url', 'reset_token')
+    
+admin.site.register(Profile, ProfileAdmin)
