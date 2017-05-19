cd29ee427bbca42ae66b8eb01920cb22df7d3b81
Trinche <m.trinchero11@gmail.com>
2017-05-19 11:00:33 +0200

    link footer added

--- dashboard/static/js/index.js
+++ dashboard/static/js/index.js
@@ -4,6 +4,8 @@ require("bootstrap-sass")
 require("bootstrap-sass/assets/stylesheets/_bootstrap.scss")
 require("../style/dashboard.scss")
 require("angular-toastr/dist/angular-toastr.css")
+require("../../../node_modules/cookieconsent/build/cookieconsent.min.css");
+require("../../../node_modules/cookieconsent/build/cookieconsent.min");
 
 import  * as  _  from 'lodash'
 import * as angular from 'angular';
@@ -25,11 +27,10 @@ app.config(['$qProvider', function ($qProvider) {
 app.controller('baseController', require('../../../static/js/controllers/base.controller').default )
 require("../../../static/js/footer/footer")
 
+
 export { app };
 
 app.controller('dashboardController', require('./controllers/dashboard.controller').default )
 app.controller('searchController', require('./controllers/searchmembers.controller').default )
 
-require("../../../node_modules/cookieconsent/build/cookieconsent.min.css")
-require("../../../node_modules/cookieconsent/build/cookieconsent.min")
-require("../../../static/js/cookie/cookiePolicy")
+require("../../../static/js/cookie/cookiePolicy");
