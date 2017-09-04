271f1df74d8b56f80f717254edde6ac95d75ef5b
andreafspeziale <andreaf.speziale@gmail.com>
2017-09-04 16:41:28 +0200

    new angular app added for pss - toastr needs at least an angular app to work

--- pss/static/js/index.js
+++ pss/static/js/index.js
@@ -1,7 +1,18 @@
 /**
  * Created by alexcomu on 03/08/17.
  */
+import * as angular from 'angular';
 
 require("../style/index.scss")
 let baseImports = require("../../../static/js/index")
 require("./upload.behaviour")
+baseImports.angularForm()
+
+// Init Angular APP
+var app = angular.module('pss', ['ui.bootstrap','toastr'])
+    .config(['$interpolateProvider', function($interpolateProvider) {
+            $interpolateProvider.startSymbol('{$');
+            $interpolateProvider.endSymbol('$}');
+    }])
+
+baseImports.angularBase(app)
