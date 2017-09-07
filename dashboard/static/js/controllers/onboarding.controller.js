e69b5d387aba3d386a6ac84c335ea27ebae947ed
santoli-massimo <santoli.massimo@gmail.com>
2017-09-07 15:00:21 +0200

    FIXED : social link not clickable ON 'Profile' page ;

--- dashboard/static/js/controllers/onboarding.controller.js
+++ dashboard/static/js/controllers/onboarding.controller.js
@@ -55,5 +55,36 @@ export default [ '$scope', function ($scope) {
     
     $scope.fitImageToCircle($('#profile-image'))
     $scope.fitImageToCircle($('.profile-image-static img'))
+    
+    $scope.checkSocialUrl =(url)=>{
+        return (
+            url.match(/http:\/\//) || url.match(/https:\/\//) ||
+            url.match(/www\./) ||
+            url.match(/\.com/) ||
+            url.match(/\.it/) ||
+            url.match(/\.org/) ||
+            url.match(/\.net/)
+        )
+    }
+    
+    $scope.addHttpToSocialUrl =(url)=>{
+        if( !url.match(/http:\/\//) && !url.match(/https:\/\//) ) url = 'https://'+url;
+        return url
+    }
+    
+    $scope.extractUserFromSocialUrl =(url)=>{
+        
+        if( url.match(/http:\/\//) || url.match(/https:\/\//) ) url = url.replace('https://', '') && url.replace('http://', '')
+        if( url.match(/www\./)) url = url.replace('www.', '')
+        if( url.match(/www\./)) url = url.replace('www.', '')
+        if( url.match(/\.com/)) url = url.replace('.com', '')
+        if( url.match(/\.it/)) url = url.replace('.it', '')
+        if( url.match(/\.org/)) url = url.replace('.org', '')
+        if( url.match(/\.net/)) url = url.replace('.net', '')
+    
+        let user = url.replace(/\/$/, '').split('/').pop().trim()
+        return user !== '' ? user : url ;
+    }
+    
 
 }]
