59449f5de6be430ac13402c06f2faf0bfc491e20
santoli-massimo <santoli.massimo@gmail.com>
2017-09-12 10:41:27 +0200

    MODIFIED : User story get content only from openmaker.eu ON 'Dashboard' page ;

--- dashboard/static/js/directives/UserStories.directive.js
+++ dashboard/static/js/directives/UserStories.directive.js
@@ -34,23 +34,36 @@ export default [function(){
             
             $scope.stories = []
             
-            let openmaker_country = ['it', 'es', 'sk', 'uk']
+            let openmaker_country = ['it', 'es', 'sk', 'uk', 'eu']
             let openmaker_suffix = 'openmaker.eu'
             let api_suffix = 'wp-json/wp/v2'
+    
+            let url = `http://${openmaker_suffix}/${api_suffix}/posts?_embed=true`
+            $http.get(url).then(
+                res => {
+                    $scope.stories = res.data.slice(0,4).map(story=>{
+                        console.log('image', story)
+                        story.image = _.get(story, "['_embedded']['wp:featuredmedia'][0].source_url") || '/static/images/openmaker-logo.svg'
+                        return story
+                    })
+                }
+                ,
+                err => console.log('Error : ', err)
+            )
             
-            _.each(openmaker_country, country =>{
-                let url = `http://${country}.${openmaker_suffix}/${api_suffix}/posts?_embed=true`
-                $http.get(url).then(
-                    res => {
-                        res.data[0].image =
-                            _.get(res, "data[0]['_embedded']['wp:featuredmedia'][0].source_url") ||
-                            '/static/images/openmaker-logo.svg'
-                        $scope.stories.push(res.data[0])
-                    }
-                    ,
-                    err => console.log('Error : ', err)
-                )
-            })
+            // _.each(openmaker_country, country =>{
+            //     let url = `http://${country}.${openmaker_suffix}/${api_suffix}/posts?_embed=true`
+            //     $http.get(url).then(
+            //         res => {
+            //             res.data[0].image =
+            //                 _.get(res, "data[0]['_embedded']['wp:featuredmedia'][0].source_url") ||
+            //                 '/static/images/openmaker-logo.svg'
+            //             $scope.stories.push(res.data[0])
+            //         }
+            //         ,
+            //         err => console.log('Error : ', err)
+            //     )
+            // })
             
             
         }]
