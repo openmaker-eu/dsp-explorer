0a50762134ccbe8219b4ff5e92b06ec05b8791ee
alexcomu <alex.comunian@gmail.com>
2017-09-22 17:17:01 +0200

    static_root

---
+++ static_root/js/cookie/cookie.policy.behaviour.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#e3e3e3"
+            },
+            "button": {
+                "background": "transparent",
+                "text": "#db4348",
+                "border": "#db4348"
+            }
+        },
+        "position": "bottom-right",
+        "content": {
+            "href": "/privacy",
+            "message": "This site uses cookies, as explained in our Privacy Policy. If you use this site without adjusting your cookies settings, you agree to our use of cookies.\n",
+            "dismiss": "Close!",
+            "link": "Learn more"
+        }
+    })});
