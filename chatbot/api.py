88e0cfc66435d5dd971be7718ed09c2df281ba23
andreafspeziale <andreaf.speziale@gmail.com>
2017-11-03 15:35:26 +0100

    dumb response on its way

--- chatbot/api.py
+++ chatbot/api.py
@@ -2,7 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from utils.api import *
 import requests, json
 
-rasa_url = "http://localhost:5000"
+rasa_url = 'http://localhost:5000'
 
 
 @csrf_exempt
@@ -12,16 +12,20 @@ def message_to_rasa_nlu(request):
     try:
         message = request.POST['message']
     except KeyError:
-        return bad_request("Message missing")
+        return bad_request('Message missing')
 
     # call to rasa NLU
 
-    data = {"q": message}
+    data = {'q': message}
 
     try:
-        req = requests.post(url=rasa_url + "/parse", data=json.dumps(data))
+        req = requests.post(url=rasa_url + '/parse', data=json.dumps(data))
     except Exception as e:
         print e
         return error()
 
-    return success("ok", "response from rasa NLU", req.text)
+    intent = req.json()['intent']['name']
+
+    # switch case for dumb response
+
+    return success('ok', 'response from rasa NLU', req.text)
