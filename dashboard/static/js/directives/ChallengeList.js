609f39f0cb7fb4f44a2e8977f3db506af50fc85b
andreafspeziale <andreaf.speziale@gmail.com>
2018-02-07 12:22:33 +0100

    Challenges page layout
    Added function to remove or add directly an interest. Better card layout

--- dashboard/static/js/directives/ChallengeList.js
+++ dashboard/static/js/directives/ChallengeList.js
@@ -13,46 +13,51 @@ let template = `
         style="margin-bottom:1%; margin-top: 1%;"
     >
         <div class="card margin-bottom-20">
-            <a href="{$ '/challenge/'+challenge.id+'/' $}">
-                <div class="card-image" style="border-bottom:solid 1px rgba(160, 160, 160, 0.2);">
-                    <img style="min-width:100%;" ng-src="{$ challenge.picture $}" class="img-responsive">
-                </div>
-                <div class="card-content"><h5>{$ challenge.title $}</h5></div>
-                <div class="card-action" style="height: auto;">
-                    <p>{$ challenge.description | limitTo: 50 $}{$ challenge.description > 50 ? '...' : '' $}</p>
-                    <p>
-                        <i class="glyphicon glyphicon-time"></i> Dates: <br> {$ challenge.start_date | date:'dd MMMM yyyy' $} - {$ challenge.end_date | date:'dd MMMM yyyy' $}<br>
-                    </p>
-                    <p>
-                        <span ng-hide="challenge.interested.length < 0"><i class="glyphicon glyphicon-heart"></i> Interested: {$ challenge.interested.length $}</span><br>
-                    </p>
-                    <p ng-if="!challenge.closed">
-                        <i class="fa fa-unlock-alt" uib-tooltip="Challenge is Open"></i> Status:
-                        Open<br>
-                    </p>
-                    <p ng-if="challenge.closed">
-                        <i class="fa fa-lock text-grey disabled" uib-tooltip="Challenge is Closed"></i> Status:<br>
-                        Closed<br>
-                    </p>
-                    <p>
-                        <i ng-repeat="tag in challenge.tags">
-                            <strong>#</strong>
-                            <span>{$ tag.name $}</span>
-                        </i>
-                    </p>
-                    <p>
+            <div class="card-image" style="border-bottom:solid 1px rgba(160, 160, 160, 0.2);">
+                <img style="min-width:100%;" ng-src="{$ challenge.picture $}" class="img-responsive">
+            </div>
+            <div class="card-content"><h5>{$ challenge.title $}</h5></div>
+            <div class="card-action" style="height: auto;">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>{$ challenge.description | limitTo: 50 $}{$ challenge.description > 50 ? '...' : '' $}</p>
+                        <p>
+                            <i class="glyphicon glyphicon-time"></i> Dates: <br> {$ challenge.start_date | date:'dd MMMM yyyy' $} - {$ challenge.end_date | date:'dd MMMM yyyy' $}<br>
+                        </p>
+                        <p>
+                            <i class="glyphicon glyphicon-thumbs-up pointer"
+                                ng-class="{ 'text-red': is_interested(challenge), 'text-grey': !is_interested(challenge) }"
+                                ng-click="challenge.closed || click_interest(challenge)"
+                                uib-tooltip="{$ is_interested(challenge) ? 'You are interested in this challenge': 'Go to the detail page to show interest in this Challenge' $}"
+                                tooltip-placement="right"
+                            ></i>
+                            Interested: {$ challenge.interested.length $}<br>
+                        </p>
+                        <p ng-if="!challenge.closed">
+                            <i class="fa fa-unlock-alt" uib-tooltip="Challenge is Open" tooltip-placement="right"></i> Status:
+                            Open<br>
+                        </p>
+                        <p ng-if="challenge.closed">
+                            <i class="fa fa-lock text-grey disabled" uib-tooltip="Challenge is Closed" tooltip-placement="right"></i> Status:<br>
+                            Closed<br>
+                        </p>
+                        <p>
+                            <i ng-repeat="tag in challenge.tags">
+                                <strong>#</strong>
+                                <span>{$ tag.name $}</span>
+                            </i>
+                        </p>
+
+                        <p>Sponsor by: <strong>{$ challenge.company.name $}</strong></p>
+                    </div>
+                    <div class="col-md-12">
                         <hr>
-                    </p>
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 col-xs-6">
-                            <p>Sponsor by: <strong>{$ challenge.company.name $}</strong></p>
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-xs-6">
-                            <img class="img-thumbnail img-responsive" ng-src="{$ challenge.company.logo $}" alt="">
-                        </div>
+                    </div>
+                    <div class="col-md-12 text-right">
+                        <a href="{$ '/challenge/'+challenge.id+'/' $}"><p>Read more <i class="glyphicon glyphicon-new-window"></i></p></a>
                     </div>
                 </div>
-            </a>
+            </div>
         </div>
     </div>
 `
@@ -70,16 +75,7 @@ export default [function(){
             
             $scope.$watch('profileid', (new_data, old_data) => {
                 if(!$scope.profileid){
-                    Promise
-                        .all([$http.get('/api/v1.3/challenge/'), $http.get('/api/v1.3/interest_ids/')])
-                        .then(
-                            res => {
-                                $scope.challenges = res[0].data || []
-                                $scope.interested_ids = res[1].data || []
-                                $scope.$apply(()=>$(window).trigger('resize'))
-                            },
-                            err => console.log('Error: ', err)
-                        )
+                    $scope.get_data();
                 }
                 else {
                     $http.get('/api/v1.3/profile/' + $scope.profileid + '/challenge/').then(res => {
@@ -89,9 +85,26 @@ export default [function(){
                     })
                 }
             })
+
+            $scope.get_data = ()=> Promise
+                        .all([$http.get('/api/v1.3/challenge/'), $http.get('/api/v1.3/interest_ids/')])
+                        .then(
+                            res => {
+                                $scope.challenges = res[0].data || []
+                                $scope.interested_ids = res[1].data || []
+                                $scope.$apply(()=>$(window).trigger('resize'))
+                            },
+                            err => console.log('Error: ', err)
+                        )
+
+            $scope.click_interest = (challenge) =>
+                ($scope.is_interested(challenge) ?
+                    $http.delete('/api/v1.3/interest/challenge/'+challenge.id+'/') :
+                    $http.post('/api/v1.3/interest/challenge/'+challenge.id+'/')
+                )
+                .then(res=>$scope.get_data(),err=>console.log(err))
             
-            $scope.is_interested = challenge =>
-                $scope.interested_ids && $scope.interested_ids.indexOf(challenge.id) > -1
+            $scope.is_interested = (challenge)=>$scope.interested_ids && $scope.interested_ids.indexOf(challenge.id) > -1
  
         }]
     }
