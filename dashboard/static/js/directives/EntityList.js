f7c350e3ea60645583b65c4fb827e45097394d1e
blubecks <andreabeccaris88@gmail.com>
2018-04-09 09:43:23 +0200

    article page

--- dashboard/static/js/directives/EntityList.js
+++ dashboard/static/js/directives/EntityList.js
@@ -1,49 +1,44 @@
 import * as _ from 'lodash'
 import * as d3 from 'd3';
 let template = `
-    <div class="col-md-12" ng-if="projects.length == 0">
+    <div class="col-md-12" ng-if="entities.length == 0">
         <h2>
-            No Projects {$ is_user_logged $}
+            No Projects
         </h2>
     </div>
-    <div class="col-md-3 col-sm-3 col-xs-12"
-        ng-repeat="project in projects"
-        style="margin-bottom:1%; margin-top: 1%;">
-        <div class="card margin-bottom-20">
-            <a href="{$ '/profile/project/'+project.id+'/detail' $}" class="card-image" style="border-bottom:solid 1px rgba(160, 160, 160, 0.2);">
-                <div class="card-image" style="border-bottom:solid 1px rgba(160, 160, 160, 0.2);">
-                    <img style="min-width:100%;" ng-src="{$ project.picture $}" class="img-responsive">
-                </div>
-            </a>
-            <div class="card-content"><h5>{$ project.name $}</h5></div>
-            <div class="card-action" style="height: auto;">
-                <div class="row">
-                    <div class="col-md-12">
-                        <!--<p>{$ project.description | limitTo: 60 $} {$ feed.summary.length > 60 ? '...' : '' $}</p>-->
-                        <p>
-                            <i ng-repeat="tag in project.tags | limitTo: 2">
-                                <strong>#</strong>
-                                <span>{$ tag.name $}</span>
-                            </i>
-                            <span ng-if="project.tags.length > 2"></span>
-                        </p>
-                        <p>
-                            <i class="glyphicon glyphicon-thumbs-up"
-                                ng-class="{ 'text-red': project.is_interested, 'text-grey': !project.is_interested }"
-                                uib-tooltip="{$ project.is_interested ? 'You are interested in this project': 'Go to the detail page to show interest in this Challenge' $}"
-                                tooltip-placement="right"
-                            ></i>
-                            Interested: {$ project.interested.length $}<br>
-                        </p>
+    <div class="col-md-8 col-md-offset-1 mycontent-left">
+        <div class="col-md-3 col-sm-3 col-xs-12"
+            ng-repeat="entity in entities"
+            style="margin-bottom:1%; margin-top: 1%;">
+            <div class="card margin-bottom-20">
+                <a href="" class="card-image" style="border-bottom:solid 1px rgba(160, 160, 160, 0.2);">
+                    <div class="card-image" style="border-bottom:solid 1px rgba(160, 160, 160, 0.2);">
+                        <img style="min-width:100%;" ng-src="" class="img-responsive">
                     </div>
-                    <div class="col-md-12">
-                        <hr>
-                    </div>
-                    <div class="col-md-12 text-right">
-                        <a href="{$ '/profile/project/'+project.id+'/detail' $}"><p>Read more <i class="glyphicon glyphicon-new-window"></i></p></a>
+                </a>
+                <div class="card-content"><h5>{$ entity.title $}</h5></div>
+                <div class="card-action" style="height: auto;">
+                    <div class="row">
+                        <div class="col-md-12">
+                        </div>
+                        <div class="col-md-12">
+                            <hr>
+                        </div>
+                        <div class="col-md-12 text-right">
+                            <a href=""><p>Read more <i class="glyphicon glyphicon-new-window"></i></p></a>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+        <div class="col-md-12">
+            slider 1
+        </div>
+        <div class="col-md-12">
+            slider 2
         </div>
     </div>
 `
@@ -58,22 +53,14 @@ export default [function(){
         },
         controller : ['$scope', '$http', 'toastr', function($scope, $http, toastr) {
             let url = ''
-            $scope.projects = []
+            $scope.entities = []
             $scope.get_data = (url) => {
                 $http.get(url).then(res => {
-                    console.log(is_user_logged);
-                    console.log($scope.is_user_logged);
-                    $scope.projects = res.data.result || []
-                    $scope.projects = _.map($scope.projects, el =>{
-                        el.is_interested = _.filter(el.interested, {id:$scope.requestprofileid}).length > 0
-                        return el
-                    })
+                    $scope.entities = res.data.result || []
+                    console.log($scope.entities)
                     $scope.$apply(()=>$(window).trigger('resize'))
                 })
             }
-            console.log($scope.profileid)
-            console.log($scope.entity)
-            console.log($scope.slider)
             let id = $scope.profileid? '/'+$scope.profileid : '/'
             $scope.get_data('/api/v1.4/' + $scope.entity + id)
 
