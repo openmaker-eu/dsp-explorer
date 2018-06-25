3157768b1383f661a18d9f7751c2a3981a011bf2
santoli-massimo <santoli.massimo@gmail.com>
2018-06-25 18:26:40 +0200

    ADDED : action icons IN question ON 'Profile Detail' page ;

--- dashboard/static/js/directives/questions/ProfileQuestions.js
+++ dashboard/static/js/directives/questions/ProfileQuestions.js
@@ -2,12 +2,37 @@ let _ = require('lodash')
 let $ = require('jquery')
 
 let template = `
+
+    <style>
+        .profile-question__actions { display:flex; flex-direction: row; justify-content: flex-end;}
+        .profile-question__actions > * { display:none; }
+        .profile-question__actions:hover > * { display:block; margin:2%;}
+    </style>
+    
     <div ng-if="questions" class="profile-questions">
         <div
             ng-repeat="question in questions"
-            class="col-md-4"
+            class="col-md-3"
         >
-            <div class="background-white" style="padding:5%; border:solid 1px #bbb;">
+            <div class="background-white" style="padding:5%; border:solid 1px #efefef;">
+                
+                <div
+                    class="profile-question__actions"
+                    style="position: absolute; top:0; right:0; bottom:0; left:0; width:100%; height:100%;"
+                >
+                    <h3
+                        class="far fa-fw text-red background-white pointer"
+                        ng-class="{'fa-eye': question.visible, 'fa-eye-slash':!question.visible }"
+                        ng-if="!question.feedback[1]"
+                        style=" position: absolute; top:5%; right:10%;"
+                        ng-click="toggle_show(question)"
+                    ></h3>
+                    <h3
+                        class="fas fa-fw fa-edit text-red background-white pointer"
+                        style="position: absolute; bottom:10%; right:10%;"
+                        ng-click="edit_question()"
+                    ></h3>
+                </div>
                 
                 <h4 class="text-brown">{$ question.question $}</h4>
                 <p>{$ question.text $}</p>
@@ -18,6 +43,7 @@ let template = `
                 <p ng-if="question.feedback[1]" class="text-brown">
                     <strong>{$ profile.data.user.first_name $}:&nbsp;</strong>&nbsp;&nbsp;{$ question.feedback[0] $}
                 </p>
+                
                 <p ng-if="question.feedback[1]" class="text-red">
                     <strong>You:&nbsp;</strong>&nbsp;&nbsp;{$ question.feedback[1] $}
                 </p>
@@ -53,6 +79,9 @@ let profile_question_directive =
     
         $rootScope.$on('authorization.refresh', ()=>{})
         console.log('Profile object', $scope.profile);
+        
+        $scope.toggle_show = (q)=>{q.visible=!q.visible}
+        $scope.edit_question = ()=>{}
     
     }]
 }
