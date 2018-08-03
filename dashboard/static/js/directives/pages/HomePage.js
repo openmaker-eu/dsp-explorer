ff5601db14f958588c78c2653fc18de7f6c4b37c
santoli-massimo <santoli.massimo@gmail.com>
2018-08-03 13:07:06 +0200

    FIXED: wrong padding in homepage for small devices ;

--- dashboard/static/js/directives/pages/HomePage.js
+++ dashboard/static/js/directives/pages/HomePage.js
@@ -5,13 +5,11 @@ let template = /*html*/`
             <h3 class="col-md-12 margin-top-2-perc"><a class="text-red cairo bold" href="/manifesto">OM MANIFESTO</a></h3>
         </div>
         <div class="row margin-top-2-perc margin-bottom-5-perc">
-            <div>
-                <div ng-repeat="entity_name in entities_names" class="col-md-4">
-                    <entity-carousel entityname="{$ entity_name $}" limit="$root.authorization<10 && 1"></entity-carousel>
-                </div>
-                <div ng-if="$root.authorization<10" class="text-center col-md-12 margin-top-5-perc">
-                    <span ng-click="open_login()" class="btn btn--white btn-lg btn--big pointer">LOAD MORE</span>
-                </div>
+            <div ng-repeat="entity_name in entities_names" class="col-md-4">
+                <entity-carousel entityname="{$ entity_name $}" limit="$root.authorization<10 && 1"></entity-carousel>
+            </div>
+            <div ng-if="$root.authorization<10" class="text-center col-md-12 margin-top-5-perc">
+                 <span ng-click="open_login()" class="btn btn--white btn-lg btn--big pointer">LOAD MORE</span>
             </div>
         </div>
     </h3>
